39179ec3458bb590beaff8eae9e6de22235286ce
Diyor Mukhamedkhanov <d.mukhamedkhanov98@gmail.com>
2021-01-02 23:45:29 +0500

    List notes command added

--- notes.js
+++ notes.js
@@ -1,15 +1,14 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-function getNotes() {
-    return "Your notes...";
-}
 
-const addNote = function (title, body) {
+const getNotes = () => {
+    return "Your notes"
+}
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+    // const duplicateNotes = notes.filter(note => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -17,15 +16,15 @@ const addNote = function (title, body) {
             body: body
         })
         saveNotes(notes)
-        console.log("New note added")
+        console.log(chalk.green.inverse("New note added!"))
     } else {
-        console.log('Note title taken')
+        console.log(chalk.red.inverse("Note title taken!"))
     }
 
 
 }
 
-const removeNote = function (title) {
+const removeNote = (title) => {
     const notes = loadNotes()
     const duplicateNotes = notes.filter(note => note.title !== title)
     if (duplicateNotes.length !== notes.length){
@@ -36,12 +35,20 @@ const removeNote = function (title) {
     }
 }
 
-const saveNotes = function (notes) {
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.yellow("Your notes"))
+    notes.forEach(note =>{
+        console.log(chalk.green(note.title), chalk.green(note.body))
+    })
+}
+
+const saveNotes = notes => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
@@ -55,5 +62,6 @@ const loadNotes = function () {
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes
 }
